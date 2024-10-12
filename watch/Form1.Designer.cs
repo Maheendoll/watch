7b0c9ccab6cb86344d06087c589984c81f34aad3
Students <Students@DESKTOP-B8AO3PN>
2024-10-12 15:52:00 +0500

    Add project files.

---
+++ watch/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace watch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hrslb = new System.Windows.Forms.Label();
+            this.minslb = new System.Windows.Forms.Label();
+            this.secslb = new System.Windows.Forms.Label();
+            this.msecslb = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // hrslb
+            // 
+            this.hrslb.AutoSize = true;
+            this.hrslb.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrslb.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.hrslb.Location = new System.Drawing.Point(52, 115);
+            this.hrslb.Name = "hrslb";
+            this.hrslb.Size = new System.Drawing.Size(103, 42);
+            this.hrslb.TabIndex = 0;
+            this.hrslb.Text = "Hour";
+            this.hrslb.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // minslb
+            // 
+            this.minslb.AutoSize = true;
+            this.minslb.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minslb.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.minslb.Location = new System.Drawing.Point(174, 115);
+            this.minslb.Name = "minslb";
+            this.minslb.Size = new System.Drawing.Size(156, 42);
+            this.minslb.TabIndex = 1;
+            this.minslb.Text = "Minutes";
+            this.minslb.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // secslb
+            // 
+            this.secslb.AutoSize = true;
+            this.secslb.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secslb.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.secslb.Location = new System.Drawing.Point(350, 115);
+            this.secslb.Name = "secslb";
+            this.secslb.Size = new System.Drawing.Size(152, 42);
+            this.secslb.TabIndex = 2;
+            this.secslb.Text = "Second";
+            this.secslb.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // msecslb
+            // 
+            this.msecslb.AutoSize = true;
+            this.msecslb.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msecslb.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.msecslb.Location = new System.Drawing.Point(519, 115);
+            this.msecslb.Name = "msecslb";
+            this.msecslb.Size = new System.Drawing.Size(224, 42);
+            this.msecslb.TabIndex = 3;
+            this.msecslb.Text = "MiniSecond";
+            this.msecslb.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Location = new System.Drawing.Point(85, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label6.Location = new System.Drawing.Point(228, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "00";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label7.Location = new System.Drawing.Point(416, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label8.Location = new System.Drawing.Point(630, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(196, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 42);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(381, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 42);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.button4.Location = new System.Drawing.Point(585, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 45);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "reset";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.msecslb);
+            this.Controls.Add(this.secslb);
+            this.Controls.Add(this.minslb);
+            this.Controls.Add(this.hrslb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hrslb;
+        private System.Windows.Forms.Label minslb;
+        private System.Windows.Forms.Label secslb;
+        private System.Windows.Forms.Label msecslb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
